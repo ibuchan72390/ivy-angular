e81fe4125e336a6376ed10bbd25b729462833867
Ian Buchan <ibuchan72390@gmail.com>
2018-04-09 13:56:45 -0700

    Update the animationHelper so that we can create a slide-left animation with display: none.

--- src/Ivy.Angular.AnimationHelpers/src/animation-helper.const.ts
+++ src/Ivy.Angular.AnimationHelpers/src/animation-helper.const.ts
@@ -22,11 +22,31 @@ export class AnimationHelper {
         ]);
     }
 
+    static genSlideLeftHide(key: string, speedInMs: number): AnimationTriggerMetadata {
+
+        return trigger(key, [
+            state('0', style({ transform: 'translate3d(0, 0, 0)', display: '' })),
+            state('1', style({ transform: 'translate3d(-1000%, 0, 0)', display: 'none' })),
+            transition('0 => 1', animate(speedInMs + 'ms ease-in')),
+            transition('1 => 0', animate(speedInMs + 'ms ease-out'))
+        ]);
+    }
+
     static genSlideRight(key: string, speedInMs: number): AnimationTriggerMetadata {
 
         return trigger(key, [
             state('0', style({ right: 'translate3d(0, 0, 0)' })),
             state('1', style({ right: 'translate3d(1000%, 0, 0)' })),
+            transition('0 => 1', animate(speedInMs + 'ms ease-in')),
+            transition('1 => 0', animate(speedInMs + 'ms ease-out'))
+        ]);
+    }
+
+    static genSlideRightHide(key: string, speedInMs: number): AnimationTriggerMetadata {
+
+        return trigger(key, [
+            state('0', style({ right: 'translate3d(0, 0, 0)', display: ''  })),
+            state('1', style({ right: 'translate3d(1000%, 0, 0)', display: 'none'  })),
             transition('0 => 1', animate(speedInMs + 'ms ease-in')),
             transition('1 => 0', animate(speedInMs + 'ms ease-out'))
         ]);
