3eb5956d42f4b50b20ac99ca65bddd10af571847
Ian Buchan <ibuchan72390@gmail.com>
2018-04-12 11:08:54 -0700

    Note about investigating an alternate RegExp for Url validation.

--- src/Ivy.Angular.RegexValidators/src/Const/regex-validators.const.ts
+++ src/Ivy.Angular.RegexValidators/src/Const/regex-validators.const.ts
@@ -1,5 +1,6 @@
 ﻿
 export class RegexValidators {
 
+    // May want to update this to the following: https://gist.github.com/dperini/729294
     public static url: RegExp = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/;
 }
