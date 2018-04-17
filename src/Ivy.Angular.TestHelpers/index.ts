164b62b92bf2df0748d3d9c5aabe738f530263a8
Ian Buchan <ibuchan72390@gmail.com>
2018-04-17 11:52:11 -0700

    Fix some problems with the test helpers here.

--- src/Ivy.Angular.TestHelpers/index.ts
+++ src/Ivy.Angular.TestHelpers/index.ts
@@ -24,3 +24,9 @@ export { IvyAngularTestHelpersModule } from './ivy.angular.test-helpers.module';
 
 
 // Models
+
+
+
+// Static
+export { AngularTestHelper } from './src/angular-test.helper';
+export { FlexLayoutTestHelper } from './src/flex-layout-test.helper';
